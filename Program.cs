d1a98ad51ae072483dbe0e5a72cd8c64c4d4cbbe
AbdulHannan3333 <hannan67212@outlook.com>
2024-06-16 01:33:16 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2_Reversing_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = {10,20,30,40,50,60,70,80,90,100};
+            int[] reversed = new int[number.Length];
+            for (int i = 0; i < number.Length; i++) 
+            {
+                 reversed[number.Length -1 -i]= number[i];
+            }
+            Console.WriteLine("Reversed Number");
+            foreach (int item in reversed)
+            {
+                Console.Write($" {item} ");
+            }
+        }
+    }
+}
